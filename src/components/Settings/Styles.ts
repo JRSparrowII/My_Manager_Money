34b189ecc728f7f57a39b3d2ee808145be0f010f
Carlos Henrique <carloshenriquepvh@hotmail.com>
2022-09-09 18:39:23 -0400

    Configurações e novas rotas

---
+++ src/components/Settings/Styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+
+    .button{
+        width: auto;
+        height: 30rem;            
+        border-radius: 10px;
+        color: var(--text-body);
+        display: flex;
+        grid-template-columns: 1fr 1fr;
+        justify-content: center;
+        gap: 20px;               
+        
+        font-weight: 400;       
+        text-align: left;
+        line-height: 1.5rem;
+    }
+
+    
+`;
